42ead57090f14dca381981e42f18fba0864e6d6b
Xaroth Brook <devnull@xaroth.nl>
2013-07-11 14:27:46 +0200

    -Add: AdminClient wrapper around AdminConnection

--- libottdadmin2/client.py
+++ libottdadmin2/client.py
@@ -22,7 +22,86 @@ except ImportError:
     except ImportError:
         pass
 
+from .packets import *
+from .event import Event
 from .adminconnection import AdminConnection
 
 class AdminClient(AdminConnection):
-    pass
+    """
+    The AdminClient class is a wrapper around the AdminConnection, allowing
+    a user to create a bot more easy.
+    """
+    def __init_poll__(self):
+        self._pollobj = poll()
+        self._poll_registered = True
+        self._pollobj.register(self.fileno(), POLLIN | POLLERR | POLLHUP | POLLPRI)
+
+    def __deinit_poll__(self):
+        if self._poll_registered:
+            self._poll_registered = False
+            self._pollobj.unregister(self.fileno())
+
+    def poll(self, timeout = 1.0):
+        """
+        Polls the connection for a maximum of <timeout> seconds
+        """
+        events = self._pollobj.poll(timeout * POLL_MOD)
+        packets = []
+        for fileno, event in events:
+            if fileno != self.fileno():
+                continue
+            if (event & POLLIN) or (event & POLLPRI):
+                packets.append(self.recv_packet())
+            elif (event & POLLERR) or (event & POLLHUP):
+                self.force_disconnect()
+        return packets
+
+
+    def __init_events__(self):
+        super(AdminClient, self).__init_events__()
+
+        self.protocol_received = Event(self)
+        self.map_info_received = Event(self)
+
+    def __init_handlers__(self):
+        super(AdminClient, self).__init_handlers__()
+        self.disconnected += self.__deinit_poll__
+
+        self.packet_recv += self.on_packet
+
+        self.protocol_received += self.on_protocol_received
+        self.map_info_received += self.on_map_info_received
+        
+        self._packet_handlers = {}
+        self._packet_handlers[ServerProtocol.packetID] = self.protocol_received
+        self._packet_handlers[ServerWelcome.packetID] = self.map_info_received
+
+    def __init__(self):
+        super(AdminClient, self).__init__()
+        self.__init_poll__()
+
+        self.protocol_info = {}
+        self.map_info = {}        
+
+    def on_packet(self, packetType, data):
+        """
+        This is a generic handler for packets; it checks if the packetID
+        is listed in self._packet_handlers, and if so, call that funntion
+        with the data as param.
+
+        Unknown packets should work the same, however, the user will receive
+        the raw data, rather than decoded information
+        """
+        pid = packetType
+        if not isinstance(pid, (int,long)):
+            pid = packetType.packetID
+        if pid in self._packet_handlers:
+            self._packet_handlers[pid](data)
+
+    def on_protocol_received(self, data):
+        self.protocol_info = data
+
+    def on_map_info_received(self, data):
+        self.map_info = data
+
+
