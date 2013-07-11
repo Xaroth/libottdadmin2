a2779d3b63bdf11874d176c6863635541ba71145
Xaroth Brook <devnull@xaroth.nl>
2013-07-11 15:03:39 +0200

    -Add: python packaging information

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='libottdadmin2',
+    version='0.0.1',
+    author="Steven 'Xaroth' Noorbergen",
+    author_email='xaroth@opendune.org',
+    packages=['libottdadmin2'],
+    scripts=['bin/openttd-admin-rcon'],
+    url='https://github.com/xaroth/libottdadmin2',
+    license='http://creativecommons.org/licenses/by-nc-sa/3.0/',
+    description='A small library for the Admin Port interface for OpenTTD.',
+    long_description=open('README.md').read(),
+    install_requires=[
+    ],
+)
